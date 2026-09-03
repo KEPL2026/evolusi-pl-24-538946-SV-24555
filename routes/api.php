ff5da03e377bafe6122e67b1abff30f5f21f4e73
hanan <hananfijananto@gmail.com>
2026-09-03 19:10:41 +0700

    feat: implement login controller and endpoint

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
-
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
